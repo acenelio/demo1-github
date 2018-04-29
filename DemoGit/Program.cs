ea80d5ddc57a37da01073d01c65c38982b5ba5d9
Nelio Alves <acenelio@gmail.com>
2018-04-29 18:55:31 -0300

    Adding good afternoon

--- DemoGit/Program.cs
+++ DemoGit/Program.cs
@@ -5,6 +5,7 @@ namespace DemoGit {
         static void Main(string[] args) {
 
             Console.WriteLine("Good morning!");
+            Console.WriteLine("Good afternoon!");
         }
     }
 }
